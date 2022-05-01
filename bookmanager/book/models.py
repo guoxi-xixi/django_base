943b24e6c76329a94bdbeee57ca4d203fc4274a2
xixi <446837204@qq.com>
2022-05-01 18:35:36 +0800

    添加应用路由urls

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 # Create your models here.
+"""
+1. 我们的模型类 需要继承自 models.Model
+2. 系统会自动为我们添加一个主键--id
+3. 字段
+    
+    字段名=model.类型（选项）
+    
+    字段名其实就是数据表的字段名
+    字段名不要使用 python，mysql等关键字
+    
+    char（M）
+    varchar(M)
+    M 就是选项
+"""
+
 # 书籍列表类型
 class BookInfo(models.Model):
     # 创建字段，字段类型
---
+++ bookmanager/book/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from book.views import index
+
+# 这个是固定写法 urlpatterns = []
+urlpatterns = [
+    path('index/', index)
+]
