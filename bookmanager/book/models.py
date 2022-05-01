b7290ddd425652fae1a72a71a39a87e080956df3
xixi <446837204@qq.com>
2022-05-01 17:17:36 +0800

    站点管理注册模块

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -6,6 +6,10 @@ class BookInfo(models.Model):
     # 创建字段，字段类型
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        """将模型类以字符串的方式输出"""
+        return self.name
+
 # 准备人物列表信息的模型类
 class PeopleInfo(models.Model):
     name = models.CharField(max_length=10)
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -104,10 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+# 语言配置
+LANGUAGE_CODE = 'zh-Hans'   #'en-us'
+# 时区设置
+TIME_ZONE = 'Asia/Shanghai'  #'UTC'
 
 USE_I18N = True
 
